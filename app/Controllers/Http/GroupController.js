23d8d3733f0b436c5f26a2d907ac62f8cc8825ef
Cruzel28 <gin_ko28@hotmail.com>
2020-08-29 22:36:04 +0700

    update-GroupController.js

--- app/Controllers/Http/GroupController.js
+++ app/Controllers/Http/GroupController.js
@@ -1,6 +1,66 @@
 'use strict'
 
+const Database = use('Database')
+
+function numberTypeParamValidator(number){
+    if (Number.isNaN(parseInt(number)))
+    return {error : ` param : ${number} is not 
+    supported, please use number type instead.` }
+   
+    return {}
+  }
 class GroupController {
+    async index(){
+        const groups = await Database.table('groups')
+  
+        return {status: 200, 
+            error: undefined, 
+            data: groups || {}}
+      }
+
+      async show ({request}){
+        const { id } = request.params
+  
+       const validatedValue = numberTypeParamValidator(id)
+       if(validatedValue.error) 
+          return { status: 500, 
+                   error: validatedValue.error, 
+                   data: undefined}
+        
+
+        const group = await Database
+        .select('*')
+        .from('groups')
+        .where('group_id',id)
+        .first()
+  
+        return {status: 200, 
+                error: undefined, 
+                data: group || {}}
+        
+      }
+  
+      async store({request}){
+        const { name } = request.body
+        
+  
+        const missingKeys = []
+        if(!name) missingKeys.push('name')
+       
+  
+        if(missingKeys.length)
+           return {status: 422, 
+            error: `${missingKeys} is missing.`, 
+            data: undefined}
+  
+        const group = await Database
+              .table('groups')
+              .insert({name})
+  
+        return {status: 200, 
+                error: undefined, 
+                data: {name}}
+      } 
 }
 
 module.exports = GroupController
