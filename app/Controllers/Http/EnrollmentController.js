f7e832068c384871ebaf0a16e7c2d7cf1017dd8e
Cruzel28 <gin_ko28@hotmail.com>
2020-08-29 21:57:07 +0700

    update EnrollmentController.js

--- app/Controllers/Http/EnrollmentController.js
+++ app/Controllers/Http/EnrollmentController.js
@@ -17,6 +17,51 @@ class EnrollmentController {
   
         return {status: 200, error: undefined, data: enrollments || {}}
       }
+
+      async show ({request}){
+        const { id } = request.params
+  
+       const validatedValue = numberTypeParamValidator(id)
+       if(validatedValue.error) 
+          return { status: 500, 
+                   error: validatedValue.error, 
+                   data: undefined}
+        
+
+        const enrollment = await Database
+        .select('*')
+        .from('enrollments')
+        .where('enrollment_id',id)
+        .first()
+  
+        return {status: 200, 
+                error: undefined, 
+                data: enrollment || {}}
+        
+      }
+  
+      async store({request}){
+        const { mark, student_id, subject_id} = request.body
+        
+  
+        const missingKeys = []
+        if(!mark) missingKeys.push('mark')
+        if(!student_id) missingKeys.push('student_id')
+        if(!subject_id) missingKeys.push('subject_id')
+  
+        if(missingKeys.length)
+           return {status: 422, 
+            error: `${missingKeys} is missing.`, 
+            data: undefined}
+  
+        const enrollment = await Database
+              .table('enrollments')
+              .insert({mark, student_id, subject_id})
+  
+        return {status: 200, 
+                error: undefined, 
+                data: {mark,student_id,subject_id}}
+      } 
   
 
 }
