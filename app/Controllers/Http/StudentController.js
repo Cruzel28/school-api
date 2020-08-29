cf419d5aa71903f169c6be06a241e940d154ed49
Cruzel28 <gin_ko28@hotmail.com>
2020-08-29 09:17:58 +0700

    update StudentController

--- app/Controllers/Http/StudentController.js
+++ app/Controllers/Http/StudentController.js
@@ -1,6 +1,66 @@
 'use strict'
 
+const Database = use('Database')
+const Hash = use('Hash')
+
+function numberTypeParamValidator(number){
+    if (Number.isNaN(parseInt(number)))
+    return {error : ` param : ${number} 
+    is not supported, please use number type instead.` }
+   
+    return {}
+  }
 class StudentController {
+    async index(){
+        const students = await Database.table('students')
+        return {status: 200, 
+                error: undefined, 
+               data: students || {}}
+      }
+
+      async show ({request}){
+        const { id } = request.params
+  
+       const validatedValue = numberTypeParamValidator(id)
+       if(validatedValue.error) 
+          return { status: 500, 
+                  error: validatedValue.error,  
+                  data: undefined}
+          
+        const student = await Database
+        .select('*')
+        .from('students')
+        .where('student_id',id)
+        .first()
+  
+        return {status: 200, 
+                error: undefined, 
+                data: student || {}}
+      }
+
+      async store({request}){
+        const { first_name, last_name, email,password,group_id} = request.body
+        const hashPassword = await Hash.make(password)
+  
+        const missingKeys = []
+        if(!first_name) missingKeys.push('first_name')
+        if(!last_name) missingKeys.push('last_name')
+        if(!email) missingKeys.push('email')
+        if(!password) missingKeys.push('password')
+        if(!group_id) missingKeys.push('group_id')
+  
+        if(missingKeys.length)
+           return {status: 422, error: `${missingKeys} is missing.`, data: undefined}
+  
+        const students = await Database
+              .table('students')
+              .insert({first_name, last_name, email,password,group_id})
+  
+        return {status: 200, error: undefined, data: {first_name,last_name,email,password,group_id}}
+      }
+
+
+  
 }
 
 module.exports = StudentController
