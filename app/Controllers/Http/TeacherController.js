cd1ffd2d0043fb400b10be6b73ed9dfc7b06e29c
Cruzel28 <gin_ko28@hotmail.com>
2020-09-01 11:49:55 +0700

    update TeacherController

--- app/Controllers/Http/TeacherController.js
+++ app/Controllers/Http/TeacherController.js
@@ -39,6 +39,7 @@ class TeacherController {
 
     async store({request}){
       const { first_name, last_name, email} = request.body
+      
       const rules = {
         first_name:'required',
         last_name:'required',
@@ -49,7 +50,7 @@ class TeacherController {
       const validation = await Validator.validate(request.body,rules)
 
       if(validation.fails())
-      return {status: 422, error: validation.message(), data: undefined}
+      return {status: 422, error: validation.messages(), data: undefined}
 
       // const missingKeys = []
       // if(!first_name) missingKeys.push('first_name')
@@ -62,11 +63,11 @@ class TeacherController {
       // if(missingKeys.length)
       //    return {status: 422, error: `${missingKeys} is missing.`, data: undefined}
 
-      // const teacher = await Database
-      //       .table('teachers')
-      //       .insert({first_name, last_name, email})
+      const teacher = await Database
+           .table('teachers')
+            .insert({first_name, last_name, email})
 
-      // return {status: 200, error: undefined, data: {first_name,last_name,email}}
+      return {status: 200, error: undefined, data: {first_name,last_name,email}}
     }
 
     async update({request}){
