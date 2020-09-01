5252815a1dbac34198d4e2baae23279fcb285c57
Cruzel28 <gin_ko28@hotmail.com>
2020-09-01 23:32:41 +0700

    update Model-Enrollment

--- app/Models/Enrollment.js
+++ app/Models/Enrollment.js
@@ -4,6 +4,27 @@
 const Model = use('Model')
 
 class Enrollment extends Model {
+
+    static get primaryKey(){
+        return 'enrollment_id'
+    }
+
+    static get createdAtColumn(){
+        return null;
+    }
+    static get updateAtColumn(){
+        return null;
+    }
+
+    student(){
+        return this.belongsTo('App/Models/Student') 
+    }
+
+    subject(){
+        return this.belongsTo('App/Models/Subject') 
+    }
+
+
 }
 
 module.exports = Enrollment
