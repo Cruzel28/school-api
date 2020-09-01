abb494a17f2a9d11f03da907313aaafa922a6ef1
Cruzel28 <gin_ko28@hotmail.com>
2020-09-01 21:02:27 +0700

    add Models-Enrollment/Student

---
+++ app/Models/Student.js
@@ -0,0 +1,30 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Student extends Model {
+
+    static get primaryKey(){
+        return 'student_id'
+    }
+
+    static get createdAtColumn(){
+        return null;
+    }
+    static get updateAtColumn(){
+        return null;
+    }
+
+    enrollment(){
+        return this.hasMany('App/Models/Enrollment') 
+    }
+
+    group(){
+        return this.belongsTo('App/Models/Group')
+    }
+
+
+}
+
+module.exports = Student
