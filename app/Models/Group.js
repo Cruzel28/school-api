a18c4106d26ce2c49f761050c1e9e75f1af5e490
Cruzel28 <gin_ko28@hotmail.com>
2020-09-01 21:01:43 +0700

    update Model-Group

--- app/Models/Group.js
+++ app/Models/Group.js
@@ -7,6 +7,16 @@ class Group extends Model {
     static get primaryKey(){
         return 'group_id'
     }
+    static get createdAtColumn(){
+        return null;
+    }
+    static get updateAtColumn(){
+        return null;
+    }
+
+    Student(){
+        return this.hasMany('App/Models/Student')
+    }
 }
 
 module.exports = Group
