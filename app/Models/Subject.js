e3c713fc1c6d074e713c8e56129b43eadf4fc2cd
Cruzel28 <gin_ko28@hotmail.com>
2020-09-01 19:11:31 +0700

    add Models-Subject/Teacher

---
+++ app/Models/Subject.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Subject extends Model {
+    static get primaryKey(){
+        return 'subject_id'
+    }
+
+    static get createdAtColumn(){
+        return null;
+    }
+    static get updateAtColumn(){
+        return null;
+    }
+
+    teacher(){
+        return this.belongsTo('App/Models/Teacher')
+    }
+}
+
+module.exports = Subject
---
+++ app/Models/Teacher.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Teacher extends Model {
+    static get primaryKey(){
+        return 'teacher_id'
+    }
+
+    static get createdAtColumn(){
+        return null;
+    }
+    static get updateAtColumn(){
+        return null;
+    }
+
+    subject(){
+        return this.hasMany('App/Models/Subject')
+    }
+}
+
+module.exports = Teacher
